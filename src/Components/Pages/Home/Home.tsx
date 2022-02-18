a15dcc5d341d49c6af887f796b12b535fae0a2be
Duc Toan <ductoan391@gmail.com>
2022-02-18 15:42:12 +0700

    commit

--- src/Components/Pages/Home/Home.tsx
+++ src/Components/Pages/Home/Home.tsx
@@ -25,7 +25,6 @@ function Home() {
             scrollingSpeed={700}
             onLeave={onLeave}
             afterLoad={afterLoad}
-            // normalScrollElements={'.item-story'}
             render={() => {
                 return (
                     <>
